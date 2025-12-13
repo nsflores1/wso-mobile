374973ba52f49a6b247274abdd825b187d80f08e
Nathaniel Flores <71289838+nsflores1@users.noreply.github.com>
2025-12-13 15:30:42 -0500

    Fix bug with home view grid spacing

--- WSO Mobile/App/Views/HomeView.swift
+++ WSO Mobile/App/Views/HomeView.swift
@@ -12,6 +12,12 @@ struct HomeView: View {
     @StateObject private var dailyMessagesViewModel = DailyMessagesViewModel()
     @State private var searchText: String = ""
 
+    // these warnings are for the user when they're not logged in
+    @State private var facTrakWarn = false
+    @State private var dormTrakWarn = false
+    @State private var bookTrakWarn = false
+    @State private var facebookWarn = false
+
 
     // TODO: this can jump around when scrolling
     // To fix this, find some way to make a skeleton that you fill in,
@@ -40,33 +46,74 @@ struct HomeView: View {
                 }
                 // TODO: this does NOTHING, still need to implement
                 Section {
-                    HStack {
-                        VStack {
-                            Button(action: signIn) {
+                    LazyVGrid(columns: [
+                        GridItem(.flexible(), spacing: 8),
+                        GridItem(.flexible(), spacing: 8)
+                    ], spacing: 12) {
+                            Button {
+                                facTrakWarn = true
+                            } label: {
                                 Label("FacTrak", systemImage: "graduationcap")
                                     .foregroundStyle(.white, .accent)
                             }.buttonStyle(.borderedProminent)
                                 .controlSize(ControlSize.large)
-                            Spacer()
-                            Button(action: signIn) {
+                                .alert(
+                                    """
+                                    This feature hasn't been implemented yet!
+                                    Please check back soon.
+                                    """,
+                                    isPresented: $facTrakWarn
+                                ) {
+                                    Button("OK", role: .cancel) { }
+                                }
+                            Button{
+                                facebookWarn = true
+                            } label: {
                                 Label("Facebook", systemImage: "person")
                                     .foregroundStyle(.white, .accent)
                             }.buttonStyle(.borderedProminent)
                                 .controlSize(ControlSize.large)
-                        }
-                        VStack {
-                            Button(action: signIn) {
+                                .alert(
+                                    """
+                                    This feature hasn't been implemented yet!
+                                    Please check back soon.
+                                    """,
+                                    isPresented: $facebookWarn
+                                ) {
+                                    Button("OK", role: .cancel) { }
+                                }
+                            Button{
+                                dormTrakWarn = true
+                            } label: {
                                 Label("DormTrak", systemImage: "house")
                                     .foregroundStyle(.white, .accent)
                             }.buttonStyle(.borderedProminent)
                                 .controlSize(ControlSize.large)
-                            Spacer()
-                            Button(action: signIn) {
+                                .alert(
+                                    """
+                                    This feature hasn't been implemented yet!
+                                    Please check back soon.
+                                    """,
+                                    isPresented: $dormTrakWarn
+                                ) {
+                                    Button("OK", role: .cancel) { }
+                                }
+                            Button {
+                                bookTrakWarn = true
+                            } label: {
                                 Label("BookTrak", systemImage: "book.closed")
                                     .foregroundStyle(.white, .accent)
                             }.buttonStyle(.borderedProminent)
                                 .controlSize(ControlSize.large)
-                        }
+                                .alert(
+                                    """
+                                    This feature hasn't been implemented yet!
+                                    Please check back soon.
+                                    """,
+                                    isPresented: $bookTrakWarn
+                                ) {
+                                    Button("OK", role: .cancel) { }
+                                }
                     }
                 } header: {
                     HStack {
@@ -99,13 +146,20 @@ struct HomeView: View {
                 ToolbarItem(placement: .topBarTrailing) {
                     // TODO: replace with WSOLoginView() later,
                     // this is to placate Apple reviewers
-                    NavigationLink(destination: AboutView()) {
-                        Image(systemName: "person")
+                    HStack {
+                        NavigationLink(destination: MapView()) {
+                            Image(systemName: "map")
+                        }
+                        NavigationLink(destination: ProfileView()) {
+                            Image(systemName: "person")
+                        }
                     }
                 }
                 ToolbarItem(placement: .topBarLeading) {
-                    NavigationLink(destination: AboutView()) {
-                        Image(systemName: "wand.and.rays")
+                    HStack {
+                        NavigationLink(destination: AboutView()) {
+                            Image(systemName: "wand.and.rays")
+                        }
                     }
                 }
             }
