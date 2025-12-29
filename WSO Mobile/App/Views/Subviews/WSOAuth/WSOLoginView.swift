fbdf911f36fd7b883fe74cfcb913281350a3e281
Nathaniel Flores <71289838+nsflores1@users.noreply.github.com>
2025-12-28 20:57:32 -0700

    Better login view state

--- WSO Mobile/App/Views/Subviews/WSOAuth/WSOLoginView.swift
+++ WSO Mobile/App/Views/Subviews/WSOAuth/WSOLoginView.swift
@@ -6,12 +6,19 @@
 //
 
 import SwiftUI
+import Logging
 
 struct WSOLoginView: View {
-    @State private var username: String = ""
-    @State private var password: String = ""
+    @Environment(\.logger) private var logger
     @Environment(AuthManager.self) private var authManager
     @Environment(NotificationManager.self) private var notificationManager
+
+    @State private var username: String = ""
+    @State private var password: String = ""
+
+    // a handle to show the fail login screen, in case the user keeps retapping
+    @State private var showError: Bool = false
+    @State private var hideTask: Task<Void, Never>?
 
     // wondering how the user goes back to the correct screen?
     // this is a NavigationStack{}, so when we see our state updated at the
@@ -19,6 +26,12 @@ struct WSOLoginView: View {
     var body: some View {
         NavigationStack {
             VStack {
+                if showError {
+                    Text("Your password is wrong, please try again...")
+                        .foregroundStyle(.red)
+                        .transition(.opacity)
+                        .padding(.vertical, 20)
+                }
                 Text("Enter your Unix login (no email):")
                 TextField("Username", text: $username)
                     .textInputAutocapitalization(.never)
@@ -33,15 +46,42 @@ struct WSOLoginView: View {
                     .lineLimit(1)
                 Button("Login") {
                     Task {
-                        UIImpactFeedbackGenerator(style: .medium).impactOccurred()
-                        try await authManager.login(username: username, password: password)
+                        logger.info("Login is being attempted...")
+                        let generator = UINotificationFeedbackGenerator()
+                        generator.prepare()
+                        do {
+                            try await authManager.login(username: username, password: password)
+                            logger.info("Login succeeded")
+                            generator.notificationOccurred(.success)
+                        } catch {
+                            logger.info("Login failed")
+                            generator.notificationOccurred(.error)
+                            failedLogin()
+                        }
                     }
                 }
                 .buttonStyle(.borderedProminent)
-            }.padding(10)
-                .navigationTitle(Text("Login to WSO"))
-                .modifier(NavSubtitleIfAvailable(subtitle: "If you meant to use FaceID, enable it in settings"))
+            }
+            .animation(.easeInOut(duration: 0.2), value: showError)
+            .padding(10)
+            .navigationTitle(Text("Login to WSO"))
+            .modifier(NavSubtitleIfAvailable(subtitle: "If you meant to use FaceID, enable it in settings"))
         }.padding(20)
+    }
+
+    // I don't normally like to attach functions to views,
+    // but this one is so small it really should just go here
+    func failedLogin() {
+        showError = true
+
+        hideTask?.cancel()
+        hideTask = Task {
+            try? await Task.sleep(for: .seconds(5))
+            guard !Task.isCancelled else { return }
+            await MainActor.run {
+                showError = false
+            }
+        }
     }
 }
 
