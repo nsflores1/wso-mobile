e5e5a406a788db5e45a48f7af0be4c4e7416d606
Nathaniel Flores <71289838+nsflores1@users.noreply.github.com>
2026-01-19 14:37:16 -0500

    Bump build version

--- WSO Mobile/App/Views/MoreView.swift
+++ WSO Mobile/App/Views/MoreView.swift
@@ -54,7 +54,7 @@ struct MoreView: View {
                 }
                 Section {
                     Text("""
-                        You're using WSO Mobile Rewritten, version 1.2.0 (beta build 93)!
+                        You're using WSO Mobile Rewritten, version 1.2.0 (beta build 95)!
                         
                         Please send all feedback to the developers via the "Send Feedback" button.
                         
